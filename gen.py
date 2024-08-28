d1e08d06a79edc51abe3c4f9d2e1f56eea344d52
Grey <bloodecho001@gmail.com>
2024-08-28 15:34:00 +0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,24 +1,24 @@
 import os
 
-# Define common HTML components
-nav_links = [
-    {"name": "Home", "url": "index.html"},
-    {"name": "About", "url": "about.html"},
-    {"name": "Contact", "url": "contact.html"}
-]
+# Directory containing the static HTML files
+static_dir = "static"
 
-# Generate navigation HTML
-nav_html = "<nav><ul>"
-for link in nav_links:
-    nav_html += f'<li><a href="{link["url"]}">{link["name"]}</a></li>'
-nav_html += "</ul></nav>"
+# Generate navigation HTML dynamically based on existing HTML files
+def generate_nav_html():
+    html_files = [f for f in os.listdir(static_dir) if f.endswith('.html')]
+    nav_links = [{"name": os.path.splitext(file)[0].capitalize(), "url": file} for file in html_files]
+    nav_html = "<nav><ul>"
+    for link in nav_links:
+        nav_html += f'<li><a href="{link["url"]}">{link["name"]}</a></li>'
+    nav_html += "</ul></nav>"
+    return nav_html
 
 # Generate footer HTML
-footer_html = """
+footer_html = '''
 <footer>
     <p>&copy; 2024 Network Xcophtew. All rights reserved.</p>
 </footer>
-"""
+'''
 
 # Meta tag and script tag to be added
 meta_tag = '<meta name="google-adsense-account" content="ca-pub-5283042537011987">'
@@ -27,20 +27,26 @@ script_tag = '''
      crossorigin="anonymous"></script>
 '''
 
-# Directory containing the static HTML files
-static_dir = "static"
+# Font preconnect and stylesheet link
+font_preload = '''
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+'''
 
 # Function to inject content into HTML files
 def inject_content(html_file):
     with open(os.path.join(static_dir, html_file), "r") as file:
         content = file.read()
 
-    # Insert meta tag and script tag into <head>
+    # Generate the latest navigation HTML
+    nav_html = generate_nav_html()
+
+    # Insert meta tag, script tag, and font preloading into <head>
     if '<head>' in content:
         head_end_index = content.find('</head>')
         if head_end_index != -1:
-            # Insert meta tag and script tag before </head>
-            content = content[:head_end_index] + meta_tag + '\n' + script_tag + '\n' + content[head_end_index:]
+            content = content[:head_end_index] + meta_tag + '\n' + script_tag + '\n' + font_preload + '\n' + '<link rel="stylesheet" href="static/style.css">' + '\n' + content[head_end_index:]
     
     # Insert navigation and footer
     content = content.replace("<!-- NAVIGATION_PLACEHOLDER -->", nav_html)
@@ -50,9 +56,8 @@ def inject_content(html_file):
     with open(os.path.join(static_dir, html_file), "w") as file:
         file.write(content)
 
-# List of HTML files to process
-html_files = ["index.html", "about.html", "contact.html"]
+# Process each HTML file in the static directory
+html_files = [f for f in os.listdir(static_dir) if f.endswith('.html')]
 
-# Process each HTML file
 for html_file in html_files:
     inject_content(html_file)
